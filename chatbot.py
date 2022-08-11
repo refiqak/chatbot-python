5e701251a32c07a38655ce5c7b2dfe11fcae0fe5
Repi <refiqakhoiruna04@gmail.com>
2022-08-11 08:37:29 +0700

    (update)

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,6 @@
-import os
+#!/usr/bin/python3
+
+
 import aiml
 import time
 import re
@@ -7,16 +9,11 @@ BRAIN_FILE = "brain.dump"
 
 k = aiml.Kernel()
 
-# To increase the startup speed of the bot it is
-# possible to save the parsed aiml files as a
-# dump. This code checks if a dump exists and
-# otherwise loads the aiml from the xml files
-# and saves the brain dump.
-if os.path.exists(BRAIN_FILE):
-    k.bootstrap(learnFiles="std-startup.aiml", commands="load aiml b")
-    k.saveBrain(BRAIN_FILE)
-    print("Loading from brain file: " + BRAIN_FILE)
-    k.loadBrain(BRAIN_FILE)
+
+k.bootstrap(learnFiles="std-startup.aiml", commands="load aiml b")
+k.saveBrain(BRAIN_FILE)
+print("Loading from brain file: " + BRAIN_FILE)
+k.loadBrain(BRAIN_FILE)
 
 
 def response_question(question):
@@ -26,8 +23,8 @@ def response_question(question):
         return response
     except:
         return "Pertanyaan tidak ditemukan"
-# # Endless loop which passes the input to the bot and prints
-# # its response
+
+
 # while True:
 #     input_text = input("Enter the query> ")
 #     if(input_text == "quit"):
